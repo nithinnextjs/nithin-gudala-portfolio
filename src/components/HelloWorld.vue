6aa26f6122a8ff2f9c08230f5443908f5ca1ed58
nithin gudala <nithin.gudala22@gmail.com>
2024-07-14 14:50:52 -0500

    passing dist

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,41 @@
   <div class="hello">
     <h1>Hello, Welcome to my Profile, this is nithin gudala</h1>
   </div>
+  <v-card>
+    <v-tabs
+      v-model="tab"
+      bg-color="primary"
+    >
+      <v-tab value="one">Item One</v-tab>
+      <v-tab value="two">Item Two</v-tab>
+      <v-tab value="three">Item Three</v-tab>
+    </v-tabs>
+
+    <v-card-text>
+      <v-tabs-window v-model="tab">
+        <v-tabs-window-item value="one">
+          One
+        </v-tabs-window-item>
+
+        <v-tabs-window-item value="two">
+          Two
+        </v-tabs-window-item>
+
+        <v-tabs-window-item value="three">
+          Three
+        </v-tabs-window-item>
+      </v-tabs-window>
+    </v-card-text>
+  </v-card>
 </template>
+
+<script>
+  export default {
+    data: () => ({
+      tab: null,
+    }),
+  }
+</script>
 
 <script>
 export default {
