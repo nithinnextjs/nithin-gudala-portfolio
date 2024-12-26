4eef3337fe6f97158fa28ee723130137a3cfd000
nithin gudala <nithin.gudala22@gmail.com>
2024-12-25 18:28:44 -0600

    updated latest vue

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,15 @@
+const webpack = require('webpack');  // Use CommonJS syntax with require
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/nithin-gudala-portfolio/' : '/',
-  
+
   chainWebpack(config) {
+    // Adding the DefinePlugin for hydration mismatch feature flag
+    config.plugin('define')
+      .use(webpack.DefinePlugin, [{
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',
+      }]);
+
     // Handle .pdf, .docx, .txt files
     config.module
       .rule('assets')
