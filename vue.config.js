5a33570dcd4d44656d2ffa4b058374de0a10f4af
nithin gudala <nithin.gudala22@gmail.com>
2024-12-30 18:58:05 -0600

    updated vue

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 
 module.exports = {
   publicPath: '/nithin-gudala-portfolio/',
-
+  outputDir: 'dist', 
   /**
    * @param {{ plugin: (arg0: string) => { (): any; new (): any; use: { (arg0: typeof webpack.DefinePlugin, arg1: { __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: string; }[]): void; new (): any; }; }; module: { rule: (arg0: string) => { (): any; new (): any; test: { (arg0: RegExp): { (): any; new (): any; use: { (arg0: string): { (): any; new (): any; loader: { (arg0: string): { (): any; new (): any; options: { (arg0: { name: string; }): { (): any; new (): any; end: { (): void; new (): any; }; }; new (): any; }; }; new (): any; }; }; new (): any; }; }; new (): any; }; }; }; }} config
    */
