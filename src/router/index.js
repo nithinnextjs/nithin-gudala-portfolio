f171afe2c95edd8f5c588ca076def6e37e43a19a
nithin gudala <nithin.gudala22@gmail.com>
2024-12-30 22:18:38 -0600

    updated

--- src/router/index.js
+++ src/router/index.js
@@ -1,45 +1,23 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-
-// Import components
-import HomeProfile from '../components/home-profile/home-profile.vue';
+import { createRouter, createWebHistory } from 'vue-router';  // Import Vue Router functions
+import HomeProfile from '../components/home-profile/home-profile.vue';  // Import components
 import AboutDetails from '../components/about-details/about-details.vue';
 import EducationDetails from '../components/education-details/education-details.vue';
 import ExperienceDetails from '../components/experience-details/experience-details.vue';
 import ContactDetails from '../components/contact-details/contact-details.vue';
 
-// Use Vue Router
-Vue.use(Router);
-
+// Define your routes
 const routes = [
-  {
-    path: '/',  // Home path is correct
-    name: 'HomeProfile',
-    component: HomeProfile
-  },
-  {
-    path: '/about',
-    name: 'AboutDetails',
-    component: AboutDetails
-  },
-  {
-    path: '/education',
-    name: 'EducationDetails',
-    component: EducationDetails
-  },
-  {
-    path: '/experience',
-    name: 'ExperienceDetails',
-    component: ExperienceDetails
-  },
-  {
-    path: '/contact',
-    name: 'ContactDetails',
-    component: ContactDetails
-  }
+  { path: '/', name: 'HomeProfile', component: HomeProfile },
+  { path: '/about', name: 'AboutDetails', component: AboutDetails },
+  { path: '/education', name: 'EducationDetails', component: EducationDetails },
+  { path: '/experience', name: 'ExperienceDetails', component: ExperienceDetails },
+  { path: '/contact', name: 'ContactDetails', component: ContactDetails },
 ];
 
-export default new Router({
-  mode: 'history',  // Enable history mode
+// Create a router instance using createRouter
+const router = createRouter({
+  history: createWebHistory(), // Using HTML5 history mode
   routes
 });
+
+export default router;  // Export the router to be used in the app
